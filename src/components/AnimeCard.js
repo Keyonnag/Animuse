720c26ec8711863a90a5110d43e817c890f30ba3
Keyonna Garfine <keyonnagarfine@Keyonnas-Air.lan>
2023-02-28 12:41:25 -0600

    fixed styling

--- src/components/AnimeCard.js
+++ src/components/AnimeCard.js
@@ -10,6 +10,7 @@ function AnimeCard({
 	showDeleteButton,
 }) {
 	const [review, setReview] = useState('');
+	const [isTruncated, setIsTruncated] = useState(true);
 
 	const handleReviewChange = (event) => {
 		setReview(event.target.value);
@@ -26,57 +27,80 @@ function AnimeCard({
 	const handleDeleteClick = () => {
 		handleDeleteAnime(anime.title);
 	};
+
+	const toggleTruncate = () => {
+		setIsTruncated(!isTruncated);
+	};
+
+	const synopsis = isTruncated
+		? `${anime.synopsis.substring(0, 100)}...`
+		: anime.synopsis;
+
 	return (
 		<div className='card'>
-			<img
-				src={anime.image_url}
-				className='card-img-top'
-				alt={anime.title}
-			/>
-			<div className='card-body'>
-				<h5 className='card-title'>{anime.title}</h5>
-				<p className='card-text'>{anime.synopsis}</p>
-				<ul className='list-group list-group-flush'>
-					<li className='list-group-item'>
-						<strong>Episodes:</strong> {anime.episodes}
-					</li>
-					<li className='list-group-item'>
-						<strong>Score:</strong> {anime.score}
-					</li>
-				</ul>
-				{showUpdateButton && (
-					<div className='input-group mt-3'>
-						<input
-							type='text'
-							className='form-control'
-							placeholder='Enter review'
-							value={review}
-							onChange={handleReviewChange}
-						/>
-						<button
-							className='btn btn-primary'
-							onClick={handleUpdateClick}
-						>
-							Update Review
-						</button>
+			<div className='row g-0'>
+				<div className='col-5 col-sm-4'>
+					<img
+						src={anime.image_url}
+						className='img-fluid w-100'
+						alt={anime.title}
+					/>
+				</div>
+				<div className='col-7 col-sm-8'>
+					<div className='card-body'>
+						<h5 className='card-title'>{anime.title}</h5>
+						<p className='card-text'>{synopsis}</p>
+						{anime.synopsis.length > 100 && (
+							<button
+								className='btn btn-link'
+								onClick={toggleTruncate}
+							>
+								{isTruncated ? 'Read more' : 'Show less'}
+							</button>
+						)}
+						<ul className='card-text'>
+							<li className='list-group-item'>
+								<strong>Episodes:</strong> {anime.episodes}
+							</li>
+							<li className='list-group-item'>
+								<strong>Score:</strong> {anime.score}
+							</li>
+						</ul>
+						{showUpdateButton && (
+							<div className='input-group'>
+								<input
+									type='text'
+									className='form-control'
+									placeholder='Enter review'
+									value={review}
+									onChange={handleReviewChange}
+								/>
+								<button
+									className='btn btn-primary'
+									onClick={handleUpdateClick}
+								>
+									Update Review
+								</button>
+							</div>
+						)}
+						{showAddButton && (
+							<button
+								className='btn btn-secondary me-2 mt-3'
+								onClick={handleAddClick}
+							>
+								Add to Watchlist
+							</button>
+						)}
+						{showDeleteButton && (
+							<button
+								className='btn btn-danger mt-3'
+								onClick={handleDeleteClick}
+							>
+								Delete
+							</button>
+						)}
 					</div>
-				)}
-				{showAddButton && (
-					<button
-						className='btn btn-secondary me-2 mt-3'
-						onClick={handleAddClick}
-					>
-						Add to Watchlist
-					</button>
-				)}
-				{showDeleteButton && (
-					<button
-						className='btn btn-danger mt-3'
-						onClick={handleDeleteClick}
-					>
-						Delete
-					</button>
-				)}
+				</div>
 			</div>
 		</div>
 	);
